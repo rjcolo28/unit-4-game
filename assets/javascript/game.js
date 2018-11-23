2e3cd050c5bd501b9ef243c28613848e106fa8a1
Ryan Colosanti <silentcornball28@Ryans-MacBook-Pro.local>
2018-11-23 15:18:57 -0500

    Pseudo-Code

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// id = "target"
+    // generate new number afer every round
+// id = "wins"
+    // +1 with every player win
+// id = "losses"
+    // +1 with every player loss
+// id = "gemOne/Two/Three/Four"
+    // generate new value after every round
+// id = "playerScore"
+    // increase value with every click on gems
+    // reset to zero with win (=== "target") OR reset to zero with loss (> "target")
